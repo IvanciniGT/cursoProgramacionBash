c93a7e0bec773300487cdf44d207204aa46f1e9a
Ubuntu <ubuntu@ip-172-31-2-165.eu-west-1.compute.internal>
2020-11-13 09:44:43 +0000

    pipes

--- explicaciones/pipes.sh
+++ explicaciones/pipes.sh
@@ -33,10 +33,9 @@ function filtrar_pings(){
     do
         if [[ "$linea" =~ Unreachable$ ]] #Destination Host Unreachable
         then
-            printf $(rojo 'X')
+            printf $($1 $(rojo 'X'))
         else
-#            verde "Todo bien: $linea"
-          printf $(verde '.')
+          printf $( $1 $(verde '.'))
         fi
     done 
 }
@@ -47,27 +46,34 @@ function contar_fallos(){
 
     while read -r linea
     do
-        hora=$(date +%s)
-        
-        if [[ "$linea" =~ Unreachable$ ]] #Destination Host Unreachable
-        then
-            let contador+=1
-            ultimo_fallo=$(date)
+        if [[ "$linea" =~ from || "$linea" =~ Unreachable ]]
+        then 
+            hora=$(date +%s)
+            
+            if [[ "$linea" =~ Unreachable$ ]] #Destination Host Unreachable
+            then
+                let contador+=1
+                ultimo_fallo=$(date)
+            fi
+            
+            if (( $hora_ultima_salida + 5 < $hora ))
+            then
+                hora_ultima_salida=$hora
+    #            echo "ultima_salida=$hora_ultima_salida" > informe.pings
+                echo "numero_de_fallos_$1=$contador" >> informe.pings
+            fi
+            
+            echo "$linea"
         fi
-        
-        if (( $hora_ultima_salida + 5 < $hora ))
-        then
-            hora_ultima_salida=$hora
-            echo "Ultima salida:    $hora_ultima_salida" > informe.pings
-            echo "Número de fallos: $contador" >> informe.pings
-        fi
-        
-        echo "$linea"
     done 
 }
 
 
-ping -i 1 -c 50 172.17.0.2 | contar_fallos | filtrar_pings # Sincronización. No me llega el valor hasta que lo de dentro termine
+(ping -i 1 -c 20 172.17.0.2 | contar_fallos A | filtrar_pings fondo_azul) & # Sincronización. No me llega el valor hasta que lo de dentro termine
+(ping -i 1 -c 20 172.17.0.3 | contar_fallos B | filtrar_pings fondo_amarillo) & # Sincronización. No me llega el valor hasta que lo de dentro termine
+
+wait
+echo SALIENDO DEL PROGRAMA
 #echo
 #echo Número de fallos: $contador
 #echo Último fallo: $ultimo_fallo
